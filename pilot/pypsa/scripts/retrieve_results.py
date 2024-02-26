98be7b2b1323dfc247ac40e88b8b5d3159557e67
Schyska, Bruno <bruno.schyska@dlr.de>
2024-02-26 10:06:03 +0100

    REMix - pypsa benchmarking

--- pilot/pypsa/scripts/retrieve_results.py
+++ pilot/pypsa/scripts/retrieve_results.py
@@ -9,8 +9,10 @@ import pypsa
 import os
 
 TY = '2030'
-odir = '../results/'
+odir = '../results/timeseries/'
 BASEDIR = '../results/'
+
+countries = ['DE', 'DK', 'ES']
 
 idirs_pilot = sorted(glob.glob(BASEDIR+f'pilot_*TY{TY}_????.nc'))
 
@@ -20,25 +22,28 @@ for icy, pilot_f in enumerate(idirs_pilot):
     CY = pilot.snapshots.year[0]
     print(f'CY = {CY}')
 
-    Reading_f = glob.glob(BASEDIR+f'pilot_*TY{TY}_{CY}_Reading.nc')[0]
-    ninja_f = glob.glob(BASEDIR+f'pilot_*TY{TY}_{CY}_ninja.nc')[0]
+    idat_pilot = pilot.generators_t.p
 
-    shedding_gens = pilot.generators.index[pilot.generators.carrier=='Load']
+    alt_files = sorted(glob.glob(BASEDIR+f'pilot_*TY{TY}_{CY}_*.nc'))
 
-    idat_pilot = pilot.generators_t.p[shedding_gens]
-    idat_Reading = pypsa.Network(Reading_f).generators_t.p[shedding_gens]
-    idat_ninja = pypsa.Network(ninja_f).generators_t.p[shedding_gens]
-    
-    for gen_name in shedding_gens:
-        country = pilot.generators.bus[gen_name]
-        ofile = f'{odir}/{country}_shedding_TY{TY}.csv'
+    odat = {country: pd.DataFrame(index=pilot.snapshots, dtype=float) for country in countries}
 
-        df = pd.DataFrame(index=pilot.snapshots, dtype=float)
-        df['PECD'] = idat_pilot[gen_name]
-        df['Reading'] = idat_Reading[gen_name]
-        df['ninja'] = idat_ninja[gen_name]
+    for alt_f in alt_files:
+
+        alt = alt_f.split(f'_{CY}_')[-1].split('.')[0]
+
+        idat_alt = pypsa.Network(alt_f).generators_t.p
         
+        for country in countries:
+    
+            gen_name = pilot.generators.index[(pilot.generators.carrier=='Load') & (pilot.generators.bus==country)]
+    
+            odat[country]['PECD'] = idat_pilot[gen_name]
+            odat[country][alt] = idat_alt[gen_name]
+            
+    for k,v in odat.items():
+        ofile = f'{odir}/{k}_shedding_TY{TY}.csv'
         if not os.path.exists(ofile):
-            df.to_csv(ofile)
+            v.to_csv(ofile)
         else:
-            df.to_csv(ofile, mode='a', header=False)
+            v.to_csv(ofile, mode='a', header=False)
